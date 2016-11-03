2b3895b83d880675619fe05524602da1801fe93f
asdf <asdf@asdf.ru>
2016-11-03 16:27:35 +0300

    syncronized

---
+++ src/Box.java
@@ -0,0 +1,14 @@
+/**
+ * Created by olymp on 03.11.2016.
+ */
+public class Box {
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}

3cf276454efd00ed3fac63d99dd95e3abe9b5c35
asdf <asdf@asdf.ru>
2016-11-03 17:35:47 +0300

    waitNotify

---
+++ src/MainTime.java
@@ -0,0 +1,39 @@
+import java.util.Objects;
+
+/**
+ * Created by olymp on 03.11.2016.
+ */
+public class MainTime extends Thread {
+    public MainTime(Object globalMonitor) {
+        this.globalMonitor = globalMonitor;
+    }
+
+    Object globalMonitor=null;
+
+    private int secs=0;
+
+    public int getSecs() {
+        return secs;
+    }
+
+    public void setSecs(int secs) {
+        this.secs = secs;
+    }
+    @Override
+    public void run() {
+        while (!interrupted()) {
+
+
+            try {
+                Thread.sleep(1_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (globalMonitor) {
+                System.out.println("Spend " + ++secs + " seconds");
+                globalMonitor.notifyAll();
+
+            }
+        }
+    }
+}
--- src/NewClass.java
+++ src/NewClass.java
@@ -14,25 +14,34 @@ public class NewClass {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        Object globaoMonitor = new Object();
-        SysOut sysOut=new SysOut(globaoMonitor);
-        sysOut.start();
-        calculate();
-//        sysOut.interrupt();
-        synchronized (globaoMonitor) {
-            SysOut.predicate=false;
-            globaoMonitor.notify();
 
-        }
+        Object globalMonitor = new Object();
+        MainTime mainTime = new MainTime(globalMonitor);
+        mainTime.start();
+        SpendTime spendTime = new SpendTime(5,globalMonitor);
+        spendTime.start();
+        SpendTime spendTime1 = new SpendTime(7,globalMonitor);
+        spendTime1.start();
 
-        }
-
-        public static void calculate() {
-            try {
-                Thread.sleep(5_000);
-                System.out.println("Calculate is over");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+//        Object globaoMonitor = new Object();
+//        SysOut sysOut=new SysOut(globaoMonitor);
+//        sysOut.start();
+//        calculate();
+////        sysOut.interrupt();
+//        synchronized (globaoMonitor) {
+//            SysOut.predicate=false;
+//            globaoMonitor.notify();
+//
+//        }
+//
+//        }
+//
+//        public static void calculate() {
+//            try {
+//                Thread.sleep(5_000);
+//                System.out.println("Calculate is over");
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
         }
     }

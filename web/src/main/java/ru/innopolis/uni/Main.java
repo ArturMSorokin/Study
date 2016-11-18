183550d76cd6b5eac7df2263ba76a857f69b31d0
asdf <asdf@asdf.ru>
2016-11-18 17:29:57 +0300

    first page

---
+++ web/src/main/java/ru/innopolis/uni/Main.java
@@ -0,0 +1,10 @@
+package ru.innopolis.uni;
+
+/**
+ * Created by olymp on 18.11.2016.
+ */
+public class Main {
+    public static void main() {
+
+    }
+}
---
+++ web1/src/main/java/ru/innopolis/uni/MyServlet.java
@@ -0,0 +1,19 @@
+package ru.innopolis.uni;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by olymp on 18.11.2016.
+ */
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        OutputStream os = resp.getOutputStream();
+        os.write("<html><body>Hello, world!</body></html>".getBytes());
+    }
+}

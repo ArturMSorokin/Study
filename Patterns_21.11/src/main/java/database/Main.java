7a8002f4d5813c4dec5b8060429178f31cdcc0f9
asdf <asdf@asdf.ru>
2016-11-22 15:26:38 +0300

    database

--- Patterns_21.11/src/main/java/database/Main.java
+++ Patterns_21.11/src/main/java/database/Main.java
@@ -7,6 +7,7 @@ import java.sql.*;
  */
 public class Main {
     public static final String INSERT = "insert into Product (id name) values (1,'Ivan');";
+    public static final String PREPARED_INSERT = "insert into Product (id name) values (?,?');";
     public static void main(String[] arg) {
         try {
             Class.forName("org.h2.Driver");
@@ -15,7 +16,9 @@ public class Main {
             e.printStackTrace();
         }
         try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test2");
-                 Statement statement = connection.createStatement()
+                 Statement statement = connection.createStatement();
+                PreparedStatement  pStatement = connection.prepareStatement(PREPARED_INSERT);
+                Statement statement1 = connection.createStatement();
         )  {
             statement.execute("create table product(id integer primary key, name VARCHAR(255));");
             int count = statement.executeUpdate(INSERT);
@@ -24,6 +27,15 @@ public class Main {
                 System.out.println("id = "+result.getInt(1)+"    "+result.getString(2));
             }
             System.out.println("Succes, added ="+count);
+
+            for (int i=2; i<100; ++i) {
+                pStatement.setInt(1,i);
+                pStatement.setString(2,"name"+i);
+                pStatement.executeUpdate();
+            }
+            ResultSet resultSet = statement1.executeQuery("SELECT *FROM PRODUCT;");
+            while (resultSet.next())
+                System.out.println("id:"+resultSet.getInt(1)+"    name:"+resultSet.getString(2));
         } catch (SQLException e) {
             e.printStackTrace();
         }

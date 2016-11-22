e5ace07c108b582a9f37b8d1947b51f0508118d9
asdf <asdf@asdf.ru>
2016-11-22 15:18:11 +0300

    database

--- Patterns_21.11/src/main/java/database/Main.java
+++ Patterns_21.11/src/main/java/database/Main.java
@@ -1,23 +1,32 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * Created by olymp on 22.11.2016.
  */
 public class Main {
+    public static final String INSERT = "insert into Product (id name) values (1,'Ivan');";
     public static void main(String[] arg) {
         try {
             Class.forName("org.h2.Driver");
+
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test")) {
-            System.out.println("Succes");
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test2");
+                 Statement statement = connection.createStatement()
+        )  {
+            statement.execute("create table product(id integer primary key, name VARCHAR(255));");
+            int count = statement.executeUpdate(INSERT);
+            ResultSet result = statement.executeQuery("SELECT  * FROM PRODUCT");
+            while (result.next()) {
+                System.out.println("id = "+result.getInt(1)+"    "+result.getString(2));
+            }
+            System.out.println("Succes, added ="+count);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }

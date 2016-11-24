54a8ec7cfb7bc426f3b16204c9a1e8b340a111c9
artur.sorokin <arturmsorokin@gmail.com>
2016-11-24 16:21:52 +0300

    ioc xml first connectivity through autowire prototype

--- IocTest/src/main/java/ru/innopolis/spring/Ioc/Main.java
+++ IocTest/src/main/java/ru/innopolis/spring/Ioc/Main.java
@@ -13,6 +13,12 @@ public class Main {
 //        dataHandler.setDownloader(new FileDownloader());//in spring
 //        dataHandler.setUploader(new FileUploader());//in spring
         DataHandler dataHandler = (DataHandler)applicationContext.getBean("dataHandler");
+        DataHandler dataHandler1 = (DataHandler)applicationContext.getBean("dataHandler");
+        DataHandler dataHandler2 = new DataHandler();
+        DataHandler dataHandler3 = (DataHandler)new ClassPathXmlApplicationContext(new String[]{"appContext.xml"}).getBean("dataHandler");
+        System.out.println(dataHandler==dataHandler1);
+        System.out.println(dataHandler==dataHandler2);
+        System.out.println(dataHandler==dataHandler3);
         dataHandler.hadleData("someSrcPath","someDestPath");
 
     }

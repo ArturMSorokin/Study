7ddcff8a60c1ad6b0cae6393ec66237b2905306a
artur.sorokin <arturmsorokin@gmail.com>
2016-11-24 09:34:36 +0300

    sdf

--- purchaseList/src/main/java/ru/innopolis/uni/MyServlet.java
+++ purchaseList/src/main/java/ru/innopolis/uni/MyServlet.java
@@ -13,8 +13,6 @@ import java.io.OutputStream;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletConfig().getRequestDistpacher
-        OutputStream os = resp.getOutputStream();
-        os.write("<html><body>Hello, world!</body></html>".getBytes());
+        getServletContext().getRequestDispatcher("/registration.jsp").forward(req,resp);
     }
 }

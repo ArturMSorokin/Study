e943b22d4b21ac46f54af8c84debf018f24fe7b3
MyName <you@example.com>
2016-11-25 12:40:33 +0300

    Login/Registraion

--- purchaseList/src/main/java/ru/innopolis/uni/RegisterServlet.java
+++ purchaseList/src/main/java/ru/innopolis/uni/RegisterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.sql.*;
 
 /**
  * Created by olymp on 18.11.2016.
@@ -24,8 +25,44 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        try {
+            Class.forName("org.h2.Driver");
+
+
+            try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test7");
+                 Statement statement = connection.createStatement();) {
+                ForTest:
+                {
+                    statement.execute("CREATE TABLE IF NOT EXISTS student(name VARCHAR(255) primary key, sex bool,groupNum INT );");
+                    statement.execute("insert into Product (surname, name) values ('sdf',true,22);");
+                    statement.execute("insert into Product (surname, name) values ('ter',false,33);");
+                }
+
+
+                ResultSet resultSet = statement.executeQuery("select * from workers;");
+                StringBuilder tableContent = new StringBuilder().append("\n");
+                while (resultSet.next())
+                    tableContent.append("<tr>\n" +
+                            "<td>"+resultSet.getString(1)+"</td>\n" +
+                            "<td>"+resultSet.getString(2)+"</td>\n" +
+                            "</tr>\n");
+                req.setAttribute("tableContent",tableContent.toString());
+                System.out.println(tableContent.toString());
+
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
         if (req.getParameter("liName")==null) {//this is login
-            
+
         } else { //this is registration
 
         }
